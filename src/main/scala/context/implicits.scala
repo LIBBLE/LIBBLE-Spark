14a5f2395ac69f9dfd8a0d46e8a316bc3759b9dc
ruxiang <aplysia_x@163.com>
2016-08-10 15:33:02 +0800

    change the default parallelism of load file

--- src/main/scala/context/implicits.scala
+++ src/main/scala/context/implicits.scala
@@ -1,17 +1,17 @@
 /**
- * Copyright (c) 2016 LIBBLE team supervised by Dr. Wu-Jun LI at Nanjing University.
- * All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
-
- * http://www.apache.org/licenses/LICENSE-2.0
-
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
+  * Copyright (c) 2016 LIBBLE team supervised by Dr. Wu-Jun LI at Nanjing University.
+  * All Rights Reserved.
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License. */
 package libble.context
 
 import libble.linalg.{DenseVector, SparseVector}
@@ -63,7 +63,7 @@ class LibContext(val sc: SparkContext) {
         val temp = item.split(":")
         (temp.head.toInt - 1, temp.last.toDouble)
       }.unzip
-      (label, term._1.toArray, term._2.toArray)
+      (label, term._1, term._2)
     }.cache()
     val d = terms.map(_._2.lastOption.getOrElse(0))
       .reduce(math.max) + 1
@@ -81,7 +81,7 @@ class LibContext(val sc: SparkContext) {
     * @return RDD[Instance]
     */
   def loadLIBBLEFile(path: String): RDD[Instance] = {
-    loadLIBBLEFile(path, sc.defaultMinPartitions)
+    loadLIBBLEFile(path, -1)
   }
 
   /**
@@ -93,8 +93,9 @@ class LibContext(val sc: SparkContext) {
     * @return RDD[Instance]
     */
   def loadLIBBLEFile(path: String, partsNum: Int): RDD[Instance] = {
-    val lines = sc.textFile(path, partsNum)
-      .map(_.trim)
+    val lines = {
+      if (partsNum > 0) sc.textFile(path, partsNum) else sc.textFile(path)
+    }.map(_.trim)
       .filter(line => !(line.isEmpty || line.startsWith("#")))
     lines.first().contains(":") match {
       case true => {
